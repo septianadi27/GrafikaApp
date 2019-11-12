015db3d104ee217a937bb68bf4d6bab7f133e8c8
Septian Adi Pratama <septian.adipratama85@gmail.com>
2019-11-12 22:11:58 +0700

    My first commit and some home menu design

---
+++ app/src/main/java/com/pendtium/grafikaapp/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.pendtium.grafikaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        int timeout = 4000; // make the activity visible for 4 seconds
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+                Intent homepage = new Intent(SplashScreen.this, MenuUtama.class);
+                startActivity(homepage);
+            }
+        }, timeout);
+    }
+}
